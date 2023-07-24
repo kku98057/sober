a1f6e2bf3054567027c772aa7044155dbc53fa48
DESKTOP-870CDBC\SUNHU <kku98057@naver.com>
2023-07-24 23:55:09 +0900

    쿠폰이벤트 페이지 로그인url변경

--- asset/js/event/coupon.js
+++ asset/js/event/coupon.js
@@ -2,7 +2,7 @@ const coupon_btn = document.querySelector(".coupon_btn");
 const token = Cookies.get("token");
 coupon_btn.addEventListener("click", () => {
   if (!token) {
-    window.location.href = "/auth/login.html";
+    window.location.href = "/auth/needlogin.html";
     return;
   } else {
     return (
